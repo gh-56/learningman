b39d3dafcb7acf4c46601e47ad9abfb50d3edc4d
LGH <dinggul56@gmail.com>
2023-11-10 09:15:04 +0900

    feat: 스프링과 리액트 연결 방법 변경 및 8080포트 출력 확인

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,7 +4,9 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 function App() {
-   const [hello, setHello] = useState('')
+   const [hello, setHello] = useState('');
+   const [test, setTest] = useState("test");
+   const [text, setText] = useState('');
 
     useEffect(() => {
         axios.get('/api/hello')
@@ -15,6 +17,10 @@ function App() {
     return (
         <div>
             백엔드에서 가져온 데이터입니다 : {hello}
+            <p>{test}</p>
+            <form>
+                <input name="text" type="text" value={text}/>
+            </form>
         </div>
     );
 }
