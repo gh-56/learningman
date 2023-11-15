f0420bb5b568ae5b169a26406182b0f70d34b2c8
song7446 <songwon199943@gmail.com>
2023-11-15 11:41:42 +0900

    메인 카드 로그인 회원가입 test

--- src/main/frontend/src/pages/Login.jsx
+++ src/main/frontend/src/pages/Login.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Login() {
+  const baseUrl = "http://localhost:8080";
+  const [memberEmail, setMemberEmail] = useState("");
+  const [memberPassword, setMemberPassword] = useState("");
+
+  const formSubmit = async (e) => {
+    await axios
+      .post(baseUrl + "/members/new", {
+        memberEmail: memberEmail,
+        memberPassword: memberPassword,
+      })
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onChangeHandlerEmail = (e) => {
+    setMemberEmail(e.target.value);
+  };
+  const onChangeHandlerPassword = (e) => {
+    setMemberPassword(e.target.value);
+  };
+
   return (
     <div>
-      <h1>로그인 페이지</h1>
+      <form onSubmit={formSubmit} method="GET">
+        <input
+          name="email"
+          type="text"
+          value={memberEmail}
+          onChange={onChangeHandlerEmail}
+        />
+        <input
+          name="password"
+          type="text"
+          value={memberPassword}
+          onChange={onChangeHandlerPassword}
+        />
+        <button type="submit">제출</button>
+      </form>
     </div>
   );
 }
+
 export default Login;
--- src/main/frontend/src/pages/Register.jsx
+++ src/main/frontend/src/pages/Register.jsx
@@ -1,21 +1,60 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Card from '../components/Card';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
 
 function Register() {
-  const [title, setTitle] = useState('');
+  const baseUrl = "http://localhost:8080";
+  const [memberName, setMemberName] = useState("");
+  const [memberEmail, setMemberEmail] = useState("");
+  const [memberPassword, setMemberPassword] = useState("");
 
-  useEffect(() => {
-    axios
-      .get('/api/hello')
-      .then((response) => setTitle(response.data))
-      .catch((error) => console.log(error));
-  }, []);
+  const formSubmit = async (e) => {
+    await axios
+      .post(baseUrl + "/members/new", {
+        memberName: memberName,
+        memberEmail: memberEmail,
+        memberPassword: memberPassword,
+      })
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onChangeHandlerName = (e) => {
+    setMemberName(e.target.value);
+  };
+  const onChangeHandlerEmail = (e) => {
+    setMemberEmail(e.target.value);
+  };
+  const onChangeHandlerPassword = (e) => {
+    setMemberPassword(e.target.value);
+  };
+
   return (
     <div>
       <h1>회원가입 페이지</h1>
-      <p>{title}</p>
-      <Card />
+      <form onSubmit={formSubmit} method="GET">
+        <input
+          name="name"
+          type="text"
+          value={memberName}
+          onChange={onChangeHandlerName}
+        />
+        <input
+          name="email"
+          type="text"
+          value={memberEmail}
+          onChange={onChangeHandlerEmail}
+        />
+        <input
+          name="password"
+          type="text"
+          value={memberPassword}
+          onChange={onChangeHandlerPassword}
+        />
+        <button type="submit">제출</button>
+      </form>
     </div>
   );
 }
