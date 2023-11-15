a2209ef3a67ef42cf6462a19daa2b86e23bdedb2
song7446 <songwon199943@gmail.com>
2023-11-15 11:20:03 +0900

    프론트에서 dto로 보내기 나누기

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,46 +2,17 @@
 
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import Header from './component/Header';
+import Login from './component/Login';
+
 
 function App() {
-    const baseUrl = "http://localhost:8080";
-   const [name, setName] = useState('');
-   const [email, setEmail] = useState('');
-   const [password, setPassword] = useState('');
-
-
-    const formSubmit = async (e) => {
-            await axios
-            .post(baseUrl + "/members", {
-                name : name,
-                email : email,
-                password : password
-            })
-            .then((res) => {
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }
-
-    const onChangeHandlerName = (e) => {
-        setName(e.target.value)
-    }
-    const onChangeHandlerEmail = (e) => {
-        setEmail(e.target.value)
-    }
-    const onChangeHandlerPassword = (e) => {
-        setPassword(e.target.value)
-    }
+    const title = "타이틀";
  
     return (
         <div>
-            <form onSubmit={formSubmit}>
-                <input name="name" type="text" value={name} onChange={onChangeHandlerName}/>
-                <input name="email" type="text" value={email} onChange={onChangeHandlerEmail}/>
-                <input name="password" type="text" value={password} onChange={onChangeHandlerPassword}/>
-                <button type='submit'>제출</button>
-            </form>
+          <Header title={title} />
+          <Login />
         </div>
     );
 }
--- src/main/java/com/quiz/learningman/controller/MemberController.java
+++ src/main/java/com/quiz/learningman/controller/MemberController.java
@@ -2,6 +2,8 @@
 package com.quiz.learningman.controller;
 
 import com.quiz.learningman.dto.MemberDto;
+import com.quiz.learningman.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 
 @RestController
+@RequiredArgsConstructor
 public class MemberController {
-    @PostMapping("/members")
+
+    private final MemberService memberService;
+
+    @PostMapping("/members/new")
     public void memberForm(@RequestBody MemberDto memberDto){
         System.out.println(memberDto);
-
     }
 }
