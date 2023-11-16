320392ee234bf24796393bbfbc6a31766c875ca2
LGH <dinggul56@gmail.com>
2023-11-16 15:56:09 +0900

    feat: 회원가입 유효성 검사

---
+++ src/main/java/com/quiz/learningman/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.quiz.learningman.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    // CORS 허용
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")   // 모든 경로
+                        .allowedMethods("*")           // 모든 method
+                        .allowedOrigins("http://localhost:3000");  // 접근 가능 출처(리액트)
+            }
+        };
+    }
+}
