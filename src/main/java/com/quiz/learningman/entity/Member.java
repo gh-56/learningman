955d56b88ec7029020a842aa89d1671819522046
song7446 <songwon199943@gmail.com>
2023-11-16 12:16:44 +0900

    sql 정보 저장

---
+++ src/main/java/com/quiz/learningman/entity/Member.java
@@ -0,0 +1,57 @@
+package com.quiz.learningman.entity;
+
+import com.quiz.learningman.constant.Role;
+import com.quiz.learningman.dto.MemberDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+/** 멤버 | Member */
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Member {
+
+    // 멤버 아이디
+    @Id
+    @Column
+    @GeneratedValue
+    private Long memberId;
+
+    // 이메일
+    @Column(unique = true)
+    private String memberEmail;
+
+    // 비밀번호
+    private String memberPassword;
+
+    // 이름
+    private String memberName;
+
+    // 역할
+    @Column(name = "role" ,length = 128)
+    private Role role;
+
+    public  static Member createMember(MemberDto memberDto /*PasswordEncoder passwordEncoder*/){
+        Member member = new Member();
+
+        member.setMemberName(memberDto.getMemberName());
+        member.setMemberEmail(memberDto.getMemberEmail());
+        // String encodedPassword = passwordEncoder.encode(memberDto.getMemberPassword());
+        // member.setMemberPassword(encodedPassword);
+        member.setMemberPassword(memberDto.getMemberPassword());
+
+        member.setRole(Role.ADMIN);
+
+        return member;
+    }
+}
+
---
+++ src/main/java/com/quiz/learningman/service/MemberService.java
@@ -0,0 +1,23 @@
+package com.quiz.learningman.service;
+
+import com.quiz.learningman.entity.Member;
+import com.quiz.learningman.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member){
+        return memberRepository.save(member);
+    }
+}
