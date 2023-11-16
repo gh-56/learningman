be4f99b55f6e20be9d937bd6bdd7c97933c2f2aa
song7446 <songwon199943@gmail.com>
2023-11-16 12:06:02 +0900

    sql 정보 저장

--- src/main/frontend/src/pages/Register.jsx
+++ src/main/frontend/src/pages/Register.jsx
@@ -1,24 +1,62 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Card from "../components/Card";
+import { useNavigate } from "react-router-dom";
+
+
 
 function Register() {
   const baseUrl = "http://localhost:8080";
   const [memberName, setMemberName] = useState("");
   const [memberEmail, setMemberEmail] = useState("");
   const [memberPassword, setMemberPassword] = useState("");
+  const [nameError, setNameError] = useState(null);
+  const [emailError, setEmailError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+  const navigate = useNavigate();
 
   const formSubmit = async (e) => {
-    await axios
-      .post(baseUrl + "/members/new", {
-        memberName: memberName,
-        memberEmail: memberEmail,
-        memberPassword: memberPassword,
-      })
-      .then((res) => {})
-      .catch((err) => {
-        console.log(err);
-      });
+    if (
+      memberName === null ||
+      memberName === "" ||
+      memberEmail === null ||
+      memberEmail === "" ||
+      memberEmail.includes("@") === false ||
+      memberPassword === null ||
+      memberPassword === ""
+    ) {
+      e.preventDefault();
+      if (memberName === null || memberName === "") {
+        setNameError("이름을 입력해주십시오");
+      } else {
+        setNameError(null);
+      }
+      if (memberEmail === null || memberEmail === "") {
+        setEmailError("이메일을 입력해주십시오");
+      } else if (memberEmail.includes("@") === false) {
+        setEmailError("이메일 형식으로 입력해주십시오");
+      } else {
+        setEmailError(null);
+      }
+      if (memberPassword === null || memberPassword === "") {
+        setPasswordError("비밀번호를 입력해주십시오");
+      } else {
+        setPasswordError(null);
+      }
+    } else {
+      await axios
+        .post(baseUrl + "/members/register", {
+          memberName: memberName,
+          memberEmail: memberEmail,
+          memberPassword: memberPassword,
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+        navigate('/login');
+    }
   };
 
   const onChangeHandlerName = (e) => {
@@ -34,26 +72,38 @@ function Register() {
   return (
     <div>
       <h1>회원가입 페이지</h1>
-      <form onSubmit={formSubmit} method="GET">
+      <form onSubmit={formSubmit}>
+        <label>이름</label>
         <input
           name="name"
           type="text"
           value={memberName}
           onChange={onChangeHandlerName}
         />
+        {nameError === null ? null : <p>{nameError}</p>}
+
+        <br />
+        <label>이메일</label>
         <input
           name="email"
           type="text"
           value={memberEmail}
           onChange={onChangeHandlerEmail}
         />
+        {emailError === null ? null : <p>{emailError}</p>}
+
+        <br />
+        <label>비밀번호</label>
         <input
           name="password"
           type="text"
           value={memberPassword}
           onChange={onChangeHandlerPassword}
         />
-        <button type="submit">제출</button>
+        {passwordError === null ? null : <p>{passwordError}</p>}
+
+        <br />
+        <button type="submit">회원가입</button>
       </form>
     </div>
   );
