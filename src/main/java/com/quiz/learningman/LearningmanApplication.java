bbf3886bd6cc7a87e7d50a387a28d27fa5b6d9fd
LGH <dinggul56@gmail.com>
2023-11-16 15:00:01 +0900

    init commit

---
+++ src/main/java/com/quiz/learningman/LearningmanApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.learningman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningmanApplication.class, args);
+	}
+
+}
