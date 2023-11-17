3f803b9ee0a132c54596a8cd12563116e1c91da5
LGH <dinggul56@gmail.com>
2023-11-17 13:42:46 +0900

    SecurityConfig 클래스 주석 및 멤버서비스 의존성 주입 제거

--- src/main/java/com/quiz/learningman/config/SecurityConfig.java
+++ src/main/java/com/quiz/learningman/config/SecurityConfig.java
@@ -9,9 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
 
-import java.util.Arrays;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
@@ -19,35 +17,9 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final MemberService memberService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        // cors 설정 추가
-//        http.cors( cors -> cors.configurationSource(request -> {
-//            CorsConfiguration configuration = new CorsConfiguration();
-//            configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-//            configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PUT","PATCH","OPTIONS"));
-//            return configuration;
-//        }));
-        // 로그인 관련 설정
-//        http.formLogin(
-//                form -> form
-//                        .loginPage("/login")  // 기본 로그인 페이지 URL을 설정
-//                        .defaultSuccessUrl("/")      // 로그인에 성공했을 때 URL
-//                        .usernameParameter("email")  // 로그인에 사용할 매개변수 username -> email
-//                        .failureUrl("/members/login/error") // 실패했을 때 보낼 URL
-//        );
-//
-//        // 로그아웃 관련 설정
-//        http.logout(
-//                logout -> logout
-//                        .logoutRequestMatcher(
-//                                // Ant 패턴 경로 문법 -> 해당 URL 접속 시 로그아웃됨
-//                                antMatcher("/members/logout"))
-//                        // 로그아웃이 성공한 경우 메인 페이지로 리다이렉트
-//                        .logoutSuccessUrl("/")
-//        );
         // auth 설정 추가
         http.authorizeHttpRequests(
                 authorize -> authorize
@@ -61,9 +33,7 @@ public class SecurityConfig {
         );
 
         http.cors(Customizer.withDefaults());
-        http.csrf(csrf -> csrf.disable()); // CSRF 끄기 (POST 요청에 csrf 필터가 있어서 컨트롤러에 도달하기 전에 403 응답이 온 것 같네요.)
+        http.csrf(csrf -> csrf.disable()); // CSRF 끄기 (POST 요청에 csrf 필터가 있어서 컨트롤러에 도달하기 전에 403 응답이 온 것으로 예상)
         return http.build();
     }
-
-
 }
