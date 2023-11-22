6dcce8bf857be8f8d42ef5de54f94141cf157ebb
LGH <dinggul56@gmail.com>
2023-11-22 09:11:51 +0900

    feat: jwt토큰 추가 진행 중

---
+++ src/main/java/com/quiz/learningman/config/JwtConfig.java
@@ -0,0 +1,66 @@
+//package com.quiz.learningman.config;
+//
+//import com.nimbusds.jose.JOSEException;
+//import com.nimbusds.jose.KeySourceException;
+//import com.nimbusds.jose.jwk.JWK;
+//import com.nimbusds.jose.jwk.JWKSelector;
+//import com.nimbusds.jose.jwk.JWKSet;
+//import com.nimbusds.jose.jwk.RSAKey;
+//import com.nimbusds.jose.jwk.source.JWKSource;
+//import com.nimbusds.jose.proc.SecurityContext;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.oauth2.jwt.JwtDecoder;
+//import org.springframework.security.oauth2.jwt.JwtEncoder;
+//import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+//import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+//
+//import java.security.KeyPair;
+//import java.security.KeyPairGenerator;
+//import java.security.NoSuchAlgorithmException;
+//import java.security.interfaces.RSAPublicKey;
+//import java.util.List;
+//import java.util.UUID;
+//
+//@Configuration
+//public class JwtConfig {
+//    public KeyPair keyPair(){
+//        // RSA 알고리즘으로 키 페어 생성
+//        try {
+//            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+//            keyPairGenerator.initialize(2048);
+//            return keyPairGenerator.generateKeyPair();
+//        } catch (NoSuchAlgorithmException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        // RSA Key 쌍 만들기 => OAuth2 리소스 서버 Nimbus 라이브러리 사용
+//        }
+//
+//    public RSAKey rsaKey(KeyPair keyPair) {
+//        return new RSAKey.Builder((RSAPublicKey) keyPair().getPublic()) // 공개키
+//                .privateKey(keyPair().getPrivate())   // 비공개키
+//                .keyID(UUID.randomUUID().toString())    // UUID를 통해 겹치지 않는 인식키
+//                .build();
+//    }
+//
+//    // RSA -> Json Web Key 소스 생성
+//    @Bean
+//    public JWKSource jwkSource(RSAKey rsaKey){
+//        JWKSet jwkSet = new JWKSet(rsaKey);
+//
+//        return (jwkSelector, context) -> jwkSelector.select(jwkSet);
+//    }
+//
+//    // JWT 키 암호화 (생성) : Nimbus 라이브러리 사용
+//    @Bean
+//    public JwtEncoder jwtEncoder(JWKSource jwkSource){
+//        return new NimbusJwtEncoder(jwkSource);
+//    }
+//
+//    // JWT 키 복호화 (검증) : Nimbus 라이브러리 사
+//    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+//        return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey()).build();
+//    }
+//
+//}
--- src/main/java/com/quiz/learningman/config/SecurityConfig.java
+++ src/main/java/com/quiz/learningman/config/SecurityConfig.java
@@ -5,10 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -16,12 +22,17 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+        // UserDetailService를 구현한 서비스 객체 등록
+        private final MemberService memberService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                http// auth 설정 추가
+        http// auth 설정 추가
         .csrf(AbstractHttpConfigurer::disable) // CSRF 끄기 (POST 요청에 csrf 필터가 있어서 컨트롤러에 도달하기 전에 403 응답이 온 것으로 예상)
         .cors(Customizer.withDefaults());
+        // OAuth 2.0 리소스 서버 사용 : JWT 요청 유효성 검사 기본 설정
+//        http.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         http
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
@@ -36,4 +47,24 @@ public class SecurityConfig {
                 );
         return http.build();
     }
+
+    // 인증 관리자 커스터마이징
+//    @Bean
+//    public AuthenticationManager authenticationManager(PasswordEncoder passwordEncoder, MemberService memberService){
+//
+//
+//        // 인증 공급자 구현체
+//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//        // 인증 공급자에 대한 비밀번호 암호화
+//        authenticationProvider.setPasswordEncoder(passwordEncoder());
+//        // 인증 공급자에서 사용하는 UserDetailService
+//        authenticationProvider.setUserDetailsService(memberService);
+//        return new ProviderManager();
+//    }
+//
+//    // 비밀번호 암호화
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
