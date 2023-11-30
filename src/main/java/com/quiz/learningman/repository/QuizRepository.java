6fc41cfebeb9bd62b3744a8d82a88c1e39147aba
LGH <dinggul56@gmail.com>
2023-11-30 15:04:31 +0900

    mergeAll (delete entity homework, book, chapter)

--- src/main/java/com/quiz/learningman/repository/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quiz.learningman.repository;
-
-import com.quiz.learningman.entity.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
