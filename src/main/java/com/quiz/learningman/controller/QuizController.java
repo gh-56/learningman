74340be740ca8b5d6daf278278a6c14824c91a81
LSY <peteral@naver.com>
2023-11-30 10:39:01 +0900

    11/30 feat quizrepository add quizservice 2-1

--- src/main/java/com/quiz/learningman/controller/QuizController.java
+++ src/main/java/com/quiz/learningman/controller/QuizController.java
@@ -4,6 +4,7 @@ import com.quiz.learningman.service.QuizService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuizController {
 
     private final QuizService quizService;
+
+    @GetMapping(value = "/book")
+    public ResponseEntity<String> getBookList() {
+        return ResponseEntity.ok(quizService.getBooks().toString());
+    }
 
     @PostMapping(value = "/book/chapter")
     public ResponseEntity<String> getChapterNames(@RequestParam("book") String book) {
